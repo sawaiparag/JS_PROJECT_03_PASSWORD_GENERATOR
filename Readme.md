66363ce7abfdfe9dc162f4e239f5be22e8a11e09
sawaiparag <sawaiparag@gmail.com>
2022-11-03 18:01:51 +0530

    Changes in Script

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# JavaScript_Project02_PassWord Generator 
+## This Repository contains JavaScript assignment. 
+
+
+kindly suggest me for suggestion and if any improvement needed.
+
+
+## 😌 Honest Time to finish the project
+For Completing this whole assignment I spend more than 12 hours. 
+
+
+# Live Link
+
+ [Please Click ME](https://password-generator-parag.netlify.app/)
+
+
+
+![Badge](https://img.shields.io/badge/JavaScript%20Project-Color__Changer-brightgreen)
+
+
+
+
+# 👀 Feedback
+
+If you have any feedback, please reach out to us at sawaiparag@gmail.com
--- script.js
+++ script.js
@@ -14,51 +14,101 @@ const randomFunc = {
     symbol: getRandomSymbol
 }
 
-clipboardEl.addEventListener('click', () => {
-//     const textspace = document.createElement("textspace");
-//     const pass = resultEl.innerText;
+// clipboardEl.addEventListener('click', () => {
 
-//     if (!pass){
-//         return;
-//     }
+  
+//  })
 
-//     textspace.value = pass;
-//     document.body.appendChild(textspace);
-//     textspace.select();
-//     document.execCommand("copy");
-//     textspace.remove();
-//     alert("Pass Copied");  
- const textarea = document.createElement("textarea");
-  const password = resultEl.innerText;
+// generateEl.addEventListener('click', () => {
+    
+// })
 
-  if (!password) {
-    return;
-  }
+// function generatePassword(lower, upper, number, symbol, length) {
+    
+// }
 
-  textarea.value = password;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand("copy");
-  textarea.remove();
-  alert("Password copied to clipboard");  
- })
+// function getRandomLower() {
+// }
+
+// function getRandomUpper() {
+// }
+
+// function getRandomNumber() {
+// }
+
+// function getRandomSymbol() {
+// }
+
+
+
+
+
+// clipboardEl.addEventListener('click', () => {
+//     // const textarea = document.createElement('textarea');
+//     const password = resultEl.innerText;
+
+//     if (!password)  { return; }
+//     navigator.clipboard.writeText(password)
+// });
 
 generateEl.addEventListener('click', () => {
-    
-})
+    const length= +lengthEl.value;
+    const hasLower= lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber= numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+    resultEl.innerText= generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+
+});
+
+//copy password
+clipboardEl.addEventListener('click', ()=>{
+    let Password = resultEl.innerHTML;
+    if (Password === "") {
+        console.log(Password, "hello");
+        return
+        alert("Please genrate Password first");  
+    }
+    navigator.clipboard.writeText(resultEl.innerHTML);
+    alert('coppied the text:' +resultEl.innerHTML);
+});
 
 function generatePassword(lower, upper, number, symbol, length) {
+    let generatePassword = "";
+    const typesCount = lower + upper + number + symbol;
     
+    const typesArr= [{ lower}, {upper}, {number}, {symbol}].filter
+    (
+        (item)=> Object.values(item)[0]);
+//selected type not included
+        if (!typesCount === 0){
+            return "";
+        }
+
+        //loop
+        for (let i=0; i< length; i += typesCount){
+            typesArr.forEach((type) => {
+                const funcName = Object.keys(type)[0];
+                generatePassword += randomFunc[funcName]();
+            });
+        }
+        const finalPassword = generatePassword.slice(0, length);
+        return finalPassword;
 }
 
 function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
+    const symbols='!@#$%^&*(){}[]=<>/,.';  
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
